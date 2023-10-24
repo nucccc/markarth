9a93f4ac237d64a282674d84f06a249bbfb4119f
nucccc <dan.dom.nuc@gmail.com>
2023-10-24 21:34:39 +0200

    began porting tests for DictTypStore

---
+++ tests/test_names_to_typs.py
@@ -0,0 +1,155 @@
+import pytest
+
+from markarth.convert.typs import names_to_typs
+from markarth.convert.typs.typs import PrimitiveCod, TypPrimitive, TypUnknown
+
+def test_dict_type_store():
+    types_dict = {
+        'a' : TypUnknown(),
+        'b' : TypPrimitive(PrimitiveCod.INT),
+        'c' : TypPrimitive(PrimitiveCod.FLOAT)
+    }
+
+    tp = names_to_typs.DictTypStore(types_dict)
+
+    a_typ = tp.get_typ('a')
+    assert a_typ is not None
+    assert type(a_typ) == TypUnknown
+
+    b_typ = tp.get_typ('b')
+    assert b_typ is not None
+    assert type(b_typ) == TypPrimitive
+    assert b_typ.is_int()
+    
+    c_typ = tp.get_typ('c')
+    assert c_typ is not None
+    assert type(c_typ) == TypPrimitive
+    assert c_typ.is_float()
+
+    d_typ = tp.get_typ('d')
+    assert d_typ is None
+
+    assert len(tp) == 3
+
+    tp.delete_name('a')
+
+    a_typ = tp.get_typ('a')
+    assert a_typ is None
+    
+    b_typ = tp.get_typ('b')
+    assert b_typ is not None
+    assert type(b_typ) == TypPrimitive
+    assert b_typ.is_int()
+    
+    c_typ = tp.get_typ('c')
+    assert c_typ is not None
+    assert type(c_typ) == TypPrimitive
+    assert c_typ.is_float()
+
+    d_typ = tp.get_typ('d')
+    assert d_typ is None
+
+    assert len(tp) == 2
+
+'''def test_wrap_type_store():
+
+    v1_types_dict = {
+        'a' : 'int',
+        'b' : 'float',
+        'c' : 'float'
+    }
+
+    v1_tp = names_to_typs.DictTypStore(v1_types_dict)
+
+    v2_types_dict = {
+        'd' : 'float',
+        'e' : 'int',
+        'f' : 'int'
+    }
+
+    v2_tp = names_to_typs.DictTypStore(v2_types_dict)
+
+    cll_types_dict = {
+        'g' : 'float',
+        'h' : 'int',
+        'i' : 'int'
+    }
+
+    cll_tp = names_to_typs.DictTypStore(cll_types_dict)
+
+    wtg = names_to_typs.WrapTypeStore([v1_tp, v2_tp], cll_tp)
+
+    assert wtg.get_callname_type('a') is None
+    assert wtg.get_callname_type('b') is None
+    assert wtg.get_callname_type('c') is None
+    assert wtg.get_callname_type('d') is None
+    assert wtg.get_callname_type('e') is None
+    assert wtg.get_callname_type('f') is None
+    assert wtg.get_callname_type('j') is None
+
+    assert wtg.get_callname_type('g') == 'float'
+    assert wtg.get_callname_type('h') == 'int'
+    assert wtg.get_callname_type('i') == 'int'
+
+    assert wtg.get_varname_type('a') == 'int'
+    assert wtg.get_varname_type('b') == 'float'
+    assert wtg.get_varname_type('c') == 'float'
+    assert wtg.get_varname_type('d') == 'float'
+    assert wtg.get_varname_type('e') == 'int'
+    assert wtg.get_varname_type('f') == 'int'
+    
+    assert wtg.get_varname_type('j') is None
+
+    assert wtg.get_varname_type('g') is None
+    assert wtg.get_varname_type('h') is None
+    assert wtg.get_varname_type('i') is None
+
+    wtg.delete_varname('a')
+
+    assert wtg.get_varname_type('a') is None
+
+    v1_tp.delete_name('a')
+
+    assert wtg.get_varname_type('a') is None
+    assert wtg.get_varname_type('b') == 'float'
+    assert wtg.get_varname_type('c') == 'float'
+    assert wtg.get_varname_type('d') == 'float'
+    assert wtg.get_varname_type('e') == 'int'
+    assert wtg.get_varname_type('f') == 'int' '''
+
+'''def test_py2cy_dict_store():
+    origin_type_store=names_to_typs.DictTypStore()
+    origin_type_store.add_type('an_int', 'int')
+    origin_type_store.add_type('a_float', 'float')
+    origin_type_store.add_type('a_whatever', 'whatever')
+    origin_type_store.add_type('a_bool', 'bool')
+
+    py2cy_map={
+        'int':'long',
+        'float':'float',
+        'bool':'char',
+    }
+    new_type_store = names_to_typs.py2cy_dict_store(
+        origin_type_store=origin_type_store,
+        py2cy_map=py2cy_map
+    )
+    assert len(new_type_store) == 3
+    assert new_type_store.size() == 3
+    assert new_type_store.get_type('an_int') == 'long'
+    assert new_type_store.get_type('a_float') == 'float'
+    assert new_type_store.get_type('a_bool') == 'char'
+
+    py2cy_map={
+        'int':'int',
+        'float':'double',
+        'bool':'char',
+    }
+    new_type_store = names_to_typs.py2cy_dict_store(
+        origin_type_store=origin_type_store,
+        py2cy_map=py2cy_map
+    )
+    assert len(new_type_store) == 3
+    assert new_type_store.size() == 3
+    assert new_type_store.get_type('an_int') == 'int'
+    assert new_type_store.get_type('a_float') == 'double'
+    assert new_type_store.get_type('a_bool') == 'char' '''
