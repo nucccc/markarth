a2375f9378cc43ab17da99e6c3a599cc0734fb3f
nucccc <dan.dom.nuc@gmail.com>
2023-11-04 12:38:02 +0100

    written first fixture for conftest

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+import ast
+
+from markarth.convert.preprocess import code_process
+
+source_code1 = '''import json
+
+A = 74
+B : int = 78
+
+C = "beh"
+
+D = 0.12
+
+def func0(a, b):
+    return a + b
+
+def func1(a : int, b : int) -> int:
+    return a * b
+
+def func2() -> bool:
+    pass'''
+
+@pytest.fixture
+def code1():# -> tuple[ast.AST, list[str]]:
+    #source_code1
+    return code_process.process_code(source_code1)
