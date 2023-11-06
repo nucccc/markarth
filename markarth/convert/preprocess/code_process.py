42f417e3387dfba7a7358f0210598f84b3089cd9
nucccc <dan.dom.nuc@gmail.com>
2023-11-06 22:30:56 +0100

    changed return types for code process

--- markarth/convert/preprocess/code_process.py
+++ markarth/convert/preprocess/code_process.py
@@ -17,13 +17,13 @@ def to_ast_codelines(code : str) -> list[str]:
     '''
     return [''] + to_codelines(code=code)
 
-def process_code(code : str) -> tuple[ast.AST, list[str]]:
+def process_code(code : str) -> tuple[ast.Module, list[str]]:
     '''
     process_code takes in input a string and returns a tuple 
     '''
     return (ast.parse(code), to_ast_codelines(code))
 
-def process_func_code(code : str) -> tuple[ast.AST, list[str]]:
+def process_func_code(code : str) -> tuple[ast.FunctionDef, list[str]]:
     '''
     process_func_code
     '''
@@ -31,7 +31,7 @@ def process_func_code(code : str) -> tuple[ast.AST, list[str]]:
     func_ast = ast_code.body[0]
     return (func_ast, codelines)
 
-def process_file(filepath : str) -> tuple[ast.AST, list[str]]:
+def process_file(filepath : str) -> tuple[ast.Module, list[str]]:
     '''
     process_file opens a python code file, returns its code being processed
     '''
