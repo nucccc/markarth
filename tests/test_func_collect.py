59e156932f28ff1c1b010a7767dc1008fe3f9b95
nucccc <dan.dom.nuc@gmail.com>
2023-11-20 22:29:23 +0100

    added test to retrieve input funcs

--- tests/test_func_collect.py
+++ tests/test_func_collect.py
@@ -2,6 +2,7 @@ import pytest
 
 from markarth.convert.collect.func_collect import (
     func_name_from_ast,
+    input_typs_from_ast,
     filter_const_candidates_at_func,
     return_typ_from_ast,
     collect_local_typs
@@ -22,6 +23,22 @@ def test_return_typ_from_func(func1, func2):
 
     func_ast2, _ = func2
     assert return_typ_from_ast(func_ast2).is_int()
+
+
+def test_input_typs_from_ast(func2):
+    func_ast2, _ = func2
+
+    input_typs = input_typs_from_ast(func_ast2)
+
+    assert len(input_typs) == 2
+    
+    a_typ = input_typs.get_typ('a')
+    assert a_typ is not None
+    assert a_typ.is_int()
+
+    b_typ = input_typs.get_typ('b')
+    assert b_typ is not None
+    assert b_typ.is_int()
 
 
 def test_func_collect1(func1):
