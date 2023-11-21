340ae46626e35827aab49c0172a2c4c335627d8f
nucccc <dan.dom.nuc@gmail.com>
2023-11-21 23:15:27 +0100

    added test for first level of const candidates

--- tests/test_mod_collect.py
+++ tests/test_mod_collect.py
@@ -5,7 +5,8 @@ testing mod colection functions
 import pytest
 
 from markarth.convert.collect.mod_collect import (
-    collect_func_defs
+    collect_func_defs,
+    collect_const_candidates
 )
 
 def test_collect_func_defs(mod3):
@@ -17,3 +18,18 @@ def test_collect_func_defs(mod3):
     assert 'f1' in func_asts.keys()
     assert 'f2' in func_asts.keys()
     assert 'f3' in func_asts.keys()
+
+def test_collect_const_candidates(mod3):
+    mod_ast, _ = mod3
+
+    const_candidates = collect_const_candidates(mod_ast = mod_ast)
+
+    assert len(const_candidates) == 2
+
+    a_typ = const_candidates.get_typ('a')
+    assert a_typ is not None
+    assert a_typ.is_int()
+
+    b_typ = const_candidates.get_typ('b')
+    assert b_typ is not None
+    assert b_typ.is_float()
