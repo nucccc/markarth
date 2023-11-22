90c3a9b1e930702e73b836c4ea84c9936695f037
nucccc <dan.dom.nuc@gmail.com>
2023-11-22 10:30:36 +0100

    removed old classes files

--- markarth/convert/cythonize/pure.py
+++ markarth/convert/cythonize/pure.py
@@ -14,7 +14,8 @@ from markarth.convert.cythonize.cy_typs import (
     cy_float_str,
     cy_int_str
 )
-
+from markarth.convert.cythonize.cy_options import FuncOpts, ModOpts
+#from markarth.convert.collect.func_collect import Func
 from markarth.convert.typs.names_to_typs import TypStore
 
 @dataclass
@@ -145,13 +146,87 @@ def store_to_cy_types(store : TypStore) -> list[CyVarType]:
     return result
 
 
-def pure_cythonize(
-    mod_ast : ast.Module,
-    codelines : list[str],
-    consts_typ_store : TypStore
-) -> str:
-    '''
-    yep maybe this should return a portion of code with cythonized stuff, in pure
-    python mode for cython 3.0
-    '''
-    is_cython_imported, alias, codeline_no = cython_imported_already(mod_ast)
+#class FuncToTypify:
+#    '''
+#    like yeah this class shall just somehow be used to add its lines to existing codelines
+#    '''
+#
+#    def __init__(self, func_collected : Func, func_opts : FuncOpts):
+#        self._c_declares_insertion_point : int = func_collected.func_ast_body[0].lineno
+#        self._collected_typs : TypStore = func_collected._local_typs
+#
+#        self._func_opts : FuncOpts = func_opts
+#
+#    
+#    def add_collected_lines(self, codelines : list[str], cy_alias : str = 'cython'):
+#        '''
+#        add_collected_lines shall take the lines generated from the collected
+#        local typs and add them to the codelines
+#        '''        
+#        cdeclares = typ_store_to_cdeclares(
+#            typ_store = self._collected_typs,
+#            default_cy_int = self._func_opts.default_int_cytyp,
+#            default_cy_float = self._func_opts.default_float_cytyp,
+#            imposed_vars = self._func_opts.imposed_vars,
+#            cy_alias = cy_alias
+#        )
+#        for cdeclare in cdeclares:
+#            codelines.insert(self._c_declares_insertion_point, cdeclare)
+#
+#
+#    def __ge__(self, __other_func : 'FuncToTypify') -> bool:
+#        return self._c_declares_insertion_point >= __other_func._c_declares_insertion_point
+#    
+#
+#    def __le__(self, __other_func : 'FuncToTypify') -> bool:
+#        return self._c_declares_insertion_point <= __other_func._c_declares_insertion_point
+#
+#
+#def funcs_to_tipify_lister(
+#    locals_collected : dict[str, TypStore],
+#    m_opts : ModOpts
+#) -> list[FuncToTypify]:
+#    '''
+#    wouldn't it be nice to have a function that actually merges what comes
+#    from options into what comes
+#
+#    yeah and it would be nice to have them already sorted
+#    '''
+#    funcs_to_tipify : list[FuncToTypify] = list()
+#    # TODO: this will probably be passed as a ditionary at a point
+#
+#    for f_name, f_coll in locals_collected.items():
+#        funcs_to_tipify.append( FuncToTypify(
+#            func_collected = f_coll,
+#            func_opts = m_opts.get_f_opt_or_default(f_name)
+#        ) )
+#
+#    funcs_to_tipify.sort(reverse=True)
+#
+#    return funcs_to_tipify
+
+
+
+
+
+#def pure_cythonize(
+#    mod_ast : ast.Module,
+#    codelines : list[str],
+#    consts_typ_store : TypStore,
+#    funcs_collected : dict[str, Func],
+#    m_opts : ModOpts
+#) -> str:
+#    '''
+#    yep maybe this should return a portion of code with cythonized stuff, in pure
+#    python mode for cython 3.0
+#    '''
+    # at first i shall check if cython is imported, and in such case i consider
+    # its alias
+#    is_cython_imported, alias, codeline_no = cython_imported_already(mod_ast)
+#
+#    funcs_to_tipify = funcs_to_tipify_lister(funcs_collected, m_opts)
+#
+#    for func_to_tipify in funcs_to_tipify:
+#        func_to_tipify.add_collected_lines(codelines)
+#
+#    return '\n'.join(codelines)
