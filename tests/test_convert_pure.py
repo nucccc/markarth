90342db65ec61ccbcacfe611909d31b6fb9e0a89
nucccc <dan.dom.nuc@gmail.com>
2023-11-23 20:59:25 +0100

    testing that convert code actually generates those lines

--- tests/test_convert_pure.py
+++ tests/test_convert_pure.py
@@ -2,8 +2,30 @@ import pytest
 
 from markarth.convert.convert_pure import convert_code
 
+from markarth.convert.collect.mod_collect import collect_func_defs
+from markarth.convert.preprocess.code_process import (
+    func_codelines,
+    process_code
+)
 
 def test_convert_pure(code_mod3):
     converted_code = convert_code(code_mod3)
 
-    print(converted_code)
+    converted_ast , converted_codelines = process_code(converted_code)
+
+    func_asts = collect_func_defs(converted_ast)
+
+    f2_codelines = func_codelines(
+        func_ast = func_asts['f2'],
+        codelines = converted_codelines
+    )
+
+    assert '    res = cython.declare(cython.int)' in f2_codelines
+
+    f3_codelines = func_codelines(
+        func_ast = func_asts['f3'],
+        codelines = converted_codelines
+    )
+
+    assert '    res = cython.declare(cython.int)' in f3_codelines
+    assert '    c = cython.declare(cython.int)' in f3_codelines
