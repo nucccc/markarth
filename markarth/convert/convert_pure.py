3aced4a58d522e9bf698a68ffef36b32b869c841
nucccc <dan.dom.nuc@gmail.com>
2023-11-24 16:29:30 +0100

    added optional options for module conversion

--- markarth/convert/convert_pure.py
+++ markarth/convert/convert_pure.py
@@ -7,12 +7,17 @@ from markarth.convert.collect.mod_collect import mod_collect
 from markarth.convert.cythonize.cy_options import ModOpts, gen_default_mod_opts
 from markarth.convert.cythonize.pure import pure_cythonize
 
-def convert_code(code : str) -> str:
+
+def convert_code(
+    code : str,
+    m_opts : ModOpts | None = None
+) -> str:
     ast_mod, codelines = code_process.process_code(code)
     
     mod_collect_res = mod_collect(ast_mod)
 
-    m_opts : ModOpts = gen_default_mod_opts()
+    if m_opts is None:
+        m_opts : ModOpts = gen_default_mod_opts()
 
     new_code = pure_cythonize(
         mod_ast = ast_mod,
