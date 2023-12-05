576dd678d6d8c0f23ddb0ee756d3294db9ffdd92
nucccc <dan.dom.nuc@gmail.com>
2023-12-05 21:55:56 +0100

    worked on coverage for func collect

--- tests/test_func_collect.py
+++ tests/test_func_collect.py
@@ -1,13 +1,18 @@
 import pytest
 
+import ast
+
 from markarth.convert.collect.func_collect import (
     func_name_from_ast,
     input_typs_from_ast,
     filter_const_candidates_at_func,
     return_typ_from_ast,
-    collect_local_typs
+    collect_local_typs,
+    collect_from_ast_body,
+    _record_vartyp,
+    CollisionEnum
 )
-from markarth.convert.typs.names_to_typs import DictTypStore
+from markarth.convert.typs.names_to_typs import DictTypStore, NamesToTyps
 from markarth.convert.typs.typs import PrimitiveCod, TypPrimitive
 
 
@@ -59,6 +64,90 @@ def test_filter_const_candidates_at_func(func2):
     assert len(global_typs) == 1
     assert 'c' not in global_typs
     assert 'd' in global_typs
+
+
+def test_record_vartyp():
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.INT)}),
+        input_typs = DictTypStore(),
+        global_typs = DictTypStore(),
+        call_typs = DictTypStore()
+    )
+
+    rec_coll = _record_vartyp('a', TypPrimitive(PrimitiveCod.INT), names_to_typs)
+    assert rec_coll == CollisionEnum.NO_COLLISION
+
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.FLOAT)}),
+        input_typs = DictTypStore(),
+        global_typs = DictTypStore(),
+        call_typs = DictTypStore()
+    )
+
+    rec_coll = _record_vartyp('a', TypPrimitive(PrimitiveCod.INT), names_to_typs)
+    assert rec_coll == CollisionEnum.NO_COLLISION
+
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore(),
+        input_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.INT)}),
+        global_typs = DictTypStore(),
+        call_typs = DictTypStore()
+    )
+
+    rec_coll = _record_vartyp('a', TypPrimitive(PrimitiveCod.INT), names_to_typs)
+    assert rec_coll == CollisionEnum.NO_COLLISION
+
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore(),
+        input_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.FLOAT)}),
+        global_typs = DictTypStore(),
+        call_typs = DictTypStore()
+    )
+
+    rec_coll = _record_vartyp('a', TypPrimitive(PrimitiveCod.INT), names_to_typs)
+    assert rec_coll == CollisionEnum.INPUT_COLLISION
+
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore(),
+        input_typs = DictTypStore(),
+        global_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.FLOAT)}),
+        call_typs = DictTypStore()
+    )
+
+    rec_coll = _record_vartyp('a', TypPrimitive(PrimitiveCod.INT), names_to_typs)
+    assert rec_coll == CollisionEnum.GLOBAL_COLLISION
+
+
+def test_collect_from_ast_body():
+    names_to_typs = NamesToTyps(
+        local_typs = DictTypStore(),
+        input_typs = DictTypStore({'a':TypPrimitive(PrimitiveCod.FLOAT)}),
+        global_typs = DictTypStore({'b':TypPrimitive(PrimitiveCod.FLOAT)}),
+        call_typs = DictTypStore()
+    )
+
+    code = '''
+a = 7
+b = 7
+'''
+
+    colliding_input_varnames : set[str] = set()
+    colliding_global_varnames : set[str] = set()
+
+    mod = ast.parse(code)
+
+    body = mod.body
+
+    collect_from_ast_body(
+        ast_body = body,
+        names_to_typs = names_to_typs,
+        colliding_input_varnames = colliding_input_varnames,
+        colliding_global_varnames = colliding_global_varnames,
+        global_varnames = set()
+    )
+
+    assert colliding_input_varnames == {'a'}
+    assert colliding_global_varnames == {'b'}
 
 
 def test_func_collect1(func1):
