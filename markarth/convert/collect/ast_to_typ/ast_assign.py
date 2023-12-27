a4732b50fef000c8c275bcb8b21c2389e42e7914
nucccc <dan.dom.nuc@gmail.com>
2023-12-27 22:58:46 +0100

    written code to start handling assignments

--- markarth/convert/collect/ast_to_typ/ast_assign.py
+++ markarth/convert/collect/ast_to_typ/ast_assign.py
@@ -1,8 +1,20 @@
 '''
 maybe one day this could be a set of stuff to elegantly handle assignments
+
+this shall assume that no weird stuff with the same variable coming up
+and out with the same name among different targets like:
+
+a = b = a, c, d = 3, 4, 5
 '''
 
 import ast
+
+from dataclasses import dataclass
+
+from markarth.convert.collect.ast_to_typ.ast_to_typ import ast_val_to_typ
+from markarth.convert.typs import typs
+from markarth.convert.typs.merge_typs import merge_typs
+from markarth.convert.typs.names_to_typs import TypStore, DictTypStore
 
 
 ASSIGN_TYPES = frozenset({
@@ -18,3 +30,70 @@ def is_assign(ast_expr : ast.AST) -> bool:
     considered an assignment
     '''
     return type(ast_expr) in ASSIGN_TYPES
+
+
+@dataclass
+class AssignTypRes:
+    '''
+    AssignTypRes represents the result of an assignment evaluation
+
+    it will have a TypStore relating variable names to the typs being assigned to
+    them, and an option annotation typ to let the above layer if an imposed
+    typ was found in annotation
+    '''
+    assigned_typs : TypStore
+    annotation : typs.Typ | None = None
+
+
+def assigned_typs(ast_expr : ast.AST) -> AssignTypRes:
+    '''
+    assigned_typs return an AssignTypRes from an assignment expression
+    '''
+    # obtaining the annotation I'm going to return, being none in case
+    # my assignment is not being annotated
+    annotation = merge_typs(ast_expr.annotation.id) if type(ast_expr) == ast.AnnAssign else None
+
+    val_typ = ast_val_to_typ(ast_expr.value)
+
+    assigned_typs : TypStore = DictTypStore()
+
+    if type(ast_expr) == ast.AnnAssign:
+        target = ast_expr.target
+        _add_target_to_typ_store(
+            target = target,
+            target_store = assigned_typs,
+            val_typ = val_typ
+        )
+    elif type(ast_expr) == ast.Assign:
+        for target in ast_expr.targets:
+            _add_target_to_typ_store(
+                target = target,
+                target_store = assigned_typs,
+                val_typ = val_typ
+            )
+    
+    return AssignTypRes(
+        assigned_typs = assigned_typs,
+        annotation = annotation
+    )
+
+
+def _add_target_to_typ_store(
+    target : ast.Name | ast.Tuple,
+    target_store : TypStore,
+    val_typ : typs.Typ
+) -> TypStore:
+    '''
+    _add_target_to_typ_store
+    '''
+    if type(target) == ast.Name:
+        var_name = target.id
+        target_store.add_typ(var_name, val_typ)
+    elif type(target) == ast.Tuple:
+        for elt in target.elts:
+            var_name = elt.id
+            # TODO: in case of a tuple at the moment i just put an any as the
+            # typ, in the future a check on eventual container typs maybe shall
+            # be done
+            target_store.add_typ(var_name, typs.TypAny())
+    return target_store
--- tests/test_ast_assign.py
+++ tests/test_ast_assign.py
@@ -1,6 +1,15 @@
 import pytest
 
-from markarth.convert.collect.ast_to_typ.ast_assign import is_assign
+import ast
+
+from markarth.convert.collect.ast_to_typ.ast_assign import (
+    is_assign,
+    assigned_typs,
+    _add_target_to_typ_store
+)
+from markarth.convert.typs.names_to_typs import DictTypStore
+from markarth.convert.typs import typs
+
 
 def test_is_assignment(mod5, mod6):
     ast_mod, _ = mod5
@@ -12,3 +21,36 @@ def test_is_assignment(mod5, mod6):
 
     for expr in ast_mod.body:
         assert not is_assign(expr)
+
+
+def test_assigned_typs():
+    pass
+
+
+def test_add_target_to_typ_store():
+    typ_store = DictTypStore()
+
+    mod = ast.parse('a = b = c = 7')
+
+    assignment = mod.body[0]
+
+    for target in assignment.targets:
+        _add_target_to_typ_store(
+            target = target,
+            target_store = typ_store,
+            val_typ = typs.TypPrimitive(typs.PrimitiveCod.INT)
+        )
+
+    assert len(typ_store) == 3
+
+    a_typ = typ_store.get_typ('a')
+    assert a_typ is not None
+    assert a_typ.is_int()
+
+    b_typ = typ_store.get_typ('b')
+    assert b_typ is not None
+    assert b_typ.is_int()
+
+    c_typ = typ_store.get_typ('c')
+    assert c_typ is not None
+    assert c_typ.is_int()
