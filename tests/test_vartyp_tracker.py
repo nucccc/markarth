757b912c869cb273af6fa8bee670e5c52f42aca6
nucccc <dan.dom.nuc@gmail.com>
2024-03-08 14:45:41 +0100

    testing precence in getting typ

--- tests/test_vartyp_tracker.py
+++ tests/test_vartyp_tracker.py
@@ -30,6 +30,10 @@ def test_vartyp_tracker_add_and_get():
 
 
 def test_vartyp_tracker_get_from_input_and_outer():
+    '''
+    test_vartyp_tracker_get_from_input_and_outer is testing whether input and
+    outer variables can be obtained
+    '''
 
     vtt = VarTypTracker(
         input_typs=DictTypStore({
@@ -54,52 +58,109 @@ def test_vartyp_tracker_get_from_input_and_outer():
     assert i1_typ_e_origin.typ.is_int()
     assert i1_typ_e_origin.origin == VarOrigin.INPUT
 
-    i1_typ = vtt.get_vartyp('i2')
-    assert i1_typ is not None
-    assert i1_typ.is_float()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('i2')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_float()
-    assert i1_typ_e_origin.origin == VarOrigin.INPUT
+    i2_typ = vtt.get_vartyp('i2')
+    assert i2_typ is not None
+    assert i2_typ.is_float()
+    i2_typ_e_origin = vtt.get_vartyp_and_origin('i2')
+    assert i2_typ_e_origin is not None
+    assert i2_typ_e_origin.typ.is_float()
+    assert i2_typ_e_origin.origin == VarOrigin.INPUT
 
-    i1_typ = vtt.get_vartyp('i3')
-    assert i1_typ is not None
-    assert i1_typ.is_int()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('i3')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_int()
-    assert i1_typ_e_origin.origin == VarOrigin.INPUT
+    i3_typ = vtt.get_vartyp('i3')
+    assert i3_typ is not None
+    assert i3_typ.is_int()
+    i3_typ_e_origin = vtt.get_vartyp_and_origin('i3')
+    assert i3_typ_e_origin is not None
+    assert i3_typ_e_origin.typ.is_int()
+    assert i3_typ_e_origin.origin == VarOrigin.INPUT
 
-    i1_typ = vtt.get_vartyp('i4')
-    assert i1_typ is not None
-    assert i1_typ.is_bool()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('i4')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_bool()
-    assert i1_typ_e_origin.origin == VarOrigin.INPUT
+    i4_typ = vtt.get_vartyp('i4')
+    assert i4_typ is not None
+    assert i4_typ.is_bool()
+    i4_typ_e_origin = vtt.get_vartyp_and_origin('i4')
+    assert i4_typ_e_origin is not None
+    assert i4_typ_e_origin.typ.is_bool()
+    assert i4_typ_e_origin.origin == VarOrigin.INPUT
 
 
     # checking for outer variables
-    i1_typ = vtt.get_vartyp('o1')
-    assert i1_typ is not None
-    assert i1_typ.is_int()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('o1')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_int()
-    assert i1_typ_e_origin.origin == VarOrigin.OUTER
+    o1_typ = vtt.get_vartyp('o1')
+    assert o1_typ is not None
+    assert o1_typ.is_int()
+    o1_typ_e_origin = vtt.get_vartyp_and_origin('o1')
+    assert o1_typ_e_origin is not None
+    assert o1_typ_e_origin.typ.is_int()
+    assert o1_typ_e_origin.origin == VarOrigin.OUTER
 
-    i1_typ = vtt.get_vartyp('o2')
-    assert i1_typ is not None
-    assert i1_typ.is_float()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('o2')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_float()
-    assert i1_typ_e_origin.origin == VarOrigin.OUTER
+    o2_typ = vtt.get_vartyp('o2')
+    assert o2_typ is not None
+    assert o2_typ.is_float()
+    o2_typ_e_origin = vtt.get_vartyp_and_origin('o2')
+    assert o2_typ_e_origin is not None
+    assert o2_typ_e_origin.typ.is_float()
+    assert o2_typ_e_origin.origin == VarOrigin.OUTER
 
-    i1_typ = vtt.get_vartyp('o3')
-    assert i1_typ is not None
-    assert i1_typ.is_str()
-    i1_typ_e_origin = vtt.get_vartyp_and_origin('o3')
-    assert i1_typ_e_origin is not None
-    assert i1_typ_e_origin.typ.is_str()
-    assert i1_typ_e_origin.origin == VarOrigin.OUTER
+    o3_typ = vtt.get_vartyp('o3')
+    assert o3_typ is not None
+    assert o3_typ.is_str()
+    o3_typ_e_origin = vtt.get_vartyp_and_origin('o3')
+    assert o3_typ_e_origin is not None
+    assert o3_typ_e_origin.typ.is_str()
+    assert o3_typ_e_origin.origin == VarOrigin.OUTER
+
+
+def test_get_vartyp_and_origin_precedence():
+    '''
+    shall test whether having a variable with overlapping names between local,
+    input and outer shall lead to a precedence
+    '''
+
+    vtt = VarTypTracker(
+        input_typs=DictTypStore({
+            'v1' : TypPrimitive(PrimitiveCod.FLOAT),
+            'v2' : TypPrimitive(PrimitiveCod.FLOAT),
+            'v4' : TypPrimitive(PrimitiveCod.FLOAT),
+        }),
+        outer_typs=DictTypStore({
+            'v1' : TypPrimitive(PrimitiveCod.BOOL),
+            'v3' : TypPrimitive(PrimitiveCod.BOOL),
+            'v4' : TypPrimitive(PrimitiveCod.BOOL),
+        })
+    )
+
+    vtt.add_local_typ('v1', TypPrimitive(PrimitiveCod.INT))
+    vtt.add_local_typ('v2', TypPrimitive(PrimitiveCod.INT))
+    vtt.add_local_typ('v3', TypPrimitive(PrimitiveCod.INT))
+
+
+    v_typ = vtt.get_vartyp('v1')
+    assert v_typ is not None
+    assert v_typ.is_int()
+    v_typ_e_origin = vtt.get_vartyp_and_origin('v1')
+    assert v_typ_e_origin is not None
+    assert v_typ_e_origin.typ.is_int()
+    assert v_typ_e_origin.origin == VarOrigin.LOCAL
+
+    v_typ = vtt.get_vartyp('v2')
+    assert v_typ is not None
+    assert v_typ.is_int()
+    v_typ_e_origin = vtt.get_vartyp_and_origin('v2')
+    assert v_typ_e_origin is not None
+    assert v_typ_e_origin.typ.is_int()
+    assert v_typ_e_origin.origin == VarOrigin.LOCAL
+
+    v_typ = vtt.get_vartyp('v3')
+    assert v_typ is not None
+    assert v_typ.is_int()
+    v_typ_e_origin = vtt.get_vartyp_and_origin('v3')
+    assert v_typ_e_origin is not None
+    assert v_typ_e_origin.typ.is_int()
+    assert v_typ_e_origin.origin == VarOrigin.LOCAL
+
+    v_typ = vtt.get_vartyp('v4')
+    assert v_typ is not None
+    assert v_typ.is_float()
+    v_typ_e_origin = vtt.get_vartyp_and_origin('v4')
+    assert v_typ_e_origin is not None
+    assert v_typ_e_origin.typ.is_float()
+    assert v_typ_e_origin.origin == VarOrigin.INPUT
